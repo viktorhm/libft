1a46e81ff4ba2d3f178556b14906104582cbfcaf
viktor <haratykviktor@gmail.com>
2023-10-25 14:50:34 +0200

    norme??

--- libft/finish/ft_putnbr_fd.c
+++ libft/finish/ft_putnbr_fd.c
@@ -3,20 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vharatyk <vharatyk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: viktor <viktor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 04:33:13 by vharatyk          #+#    #+#             */
-/*   Updated: 2023/10/25 05:37:46 by vharatyk         ###   ########.fr       */
+/*   Updated: 2023/10/25 14:49:14 by viktor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"st"
-void ft_putnbr_fd(int n, int fd)
-{
-	while(n<9)
-	{
+#include "libft.h"
 
-	write(fd,n%10 ,1);
-	n = n /10;
+
+void				ft_putnbr_fd(int n, int fd)
+{
+
+	if(n == -2147483648)
+		write(fd,"-2147483648" ,11);
+	else
+	{
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			n = n*(-1);
+		}
+		if (n >= 10)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+		else
+			ft_putchar_fd(n + '0', fd);
 	}
 }
