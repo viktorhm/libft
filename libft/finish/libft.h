ee2f7acc23f23747d6ff5f1e558bb66ed29489a5
Viktor Haratyk-Martin <vharatyk@c2r7p4.42lehavre.fr>
2023-10-26 17:38:38 +0200

    bonus

--- libft/finish/libft.h
+++ libft/finish/libft.h
@@ -6,7 +6,7 @@
 /*   By: vharatyk <vharatyk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:12:24 by vharatyk          #+#    #+#             */
-/*   Updated: 2023/10/25 17:51:24 by vharatyk         ###   ########.fr       */
+/*   Updated: 2023/10/26 17:27:10 by vharatyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,19 @@
 # include <string.h>
 # include <stddef.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+t_list		*ft_lstnew(void *content);
+t_list 		*ft_lstlast(t_list *lst);
+
 size_t		ft_strlcat(char *dest, const char *src, size_t size);
 size_t		ft_strlcpy(char *dst, const char *src, size_t size);
 size_t		ft_strlen(const char *str);
+
 
 void		*ft_memset(void *str, int value, size_t count);
 void		ft_bzero(void *str, size_t count);
@@ -32,6 +42,12 @@ void		ft_putchar_fd(char c, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
 void		ft_putstr_fd(char *s, int fd);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+
+void 		ft_lstadd_front(t_list **lst, t_list *new);
+void		ft_lstdelone(t_list *lst, void (*del)(void*));
+void 		ft_lstclear(t_list **lst, void (*del)(void*));
+void 		ft_lstiter(t_list *lst, void (*f)(void *));
 
 char		*ft_strdup(const char *s);
 char		*ft_strchr(const char *s, int c);
@@ -54,8 +70,6 @@ int			ft_isascii(int charactere);
 int			ft_isprint(int character);
 int			ft_strncmp(char *s1, char *s2, size_t n);
 int			ft_memcmp(const void *pointer1, const void *pointer2, size_t size );
-//ft_strnstr
-//ft_strlcpy
-//ft_strlcat
+int 		ft_lstsize(t_list *lst);
 
 #endif
